19e35f39a9a8a152f6cf9f68d38e312935a40b42
wangwq <wangweiqing@yunzhangfang.com>
2021-01-27 18:15:45 +0800

    gateway

---
+++ cubo-gateway/src/main/java/com/redbyte/platform/cubogateway/CuboGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.redbyte.platform.cubogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuboGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuboGatewayApplication.class, args);
+    }
+
+}
