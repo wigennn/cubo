51712bfb175d9445f17fb70ef2dbf703924231bc
wangwq <wangweiqing@yunzhangfang.com>
2021-03-04 18:21:49 +0800

    redis集群设置

--- cubo-authority-center/authority-center/src/main/java/com/redbyte/platform/authoritycenter/config/RedisClusterConfig.java
+++ cubo-authority-center/authority-center/src/main/java/com/redbyte/platform/authoritycenter/config/RedisClusterConfig.java
@@ -1,32 +1,44 @@
 package com.redbyte.platform.authoritycenter.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @author wangwq
  */
 @Configuration
+@ConditionalOnClass(JedisCluster.class)
 public class RedisClusterConfig extends CachingConfigurerSupport {
 
     @Autowired
     private RedisClusterConfigProperties redisClusterConfigProperties;
 
     @Bean
-    @Primary
-    public RedisConnectionFactory redisConnectionFactory() {
+    public JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxIdle(8);
+        config.setMaxWaitMillis(-1);
+        return config;
+    }
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
         RedisClusterConfiguration configuration = new RedisClusterConfiguration(redisClusterConfigProperties.getNodes());
         configuration.setMaxRedirects(redisClusterConfigProperties.getMaxRedirects());
-        RedisConnectionFactory factory = new JedisConnectionFactory(configuration);
+
+        JedisConnectionFactory factory = new JedisConnectionFactory(configuration, jedisPoolConfig());
         return factory;
     }
 
@@ -35,8 +47,7 @@ public class RedisClusterConfig extends CachingConfigurerSupport {
         StringRedisTemplate redisTemplate = new StringRedisTemplate();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.afterPropertiesSet();
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
         return redisTemplate;
     }
 }
--- cubo-authority-center/authority-center/src/test/java/com/redbyte/platform/authoritycenter/AuthorityCenterApplicationTests.java
+++ cubo-authority-center/authority-center/src/test/java/com/redbyte/platform/authoritycenter/AuthorityCenterApplicationTests.java
@@ -5,6 +5,11 @@ import com.redbyte.platform.authoritycenter.domain.RegisterUserDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootTest
 class AuthorityCenterApplicationTests {
@@ -33,6 +38,28 @@ class AuthorityCenterApplicationTests {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    @Test
+    void testRedis() {
+        JedisShardInfo jedis = new JedisShardInfo("139.196.205.76", 7001);
+        ShardedJedis shardedJedis = new ShardedJedis(Arrays.asList(jedis));
+
+        System.out.println(shardedJedis.get("wigen"));
+    }
+
+    @Test
+    void testRedisCluster() {
+        Set<HostAndPort> set = new HashSet<>();
+        set.add(new HostAndPort("139.196.205.76", 7001));
+        set.add(new HostAndPort("139.196.205.76", 7002));
+        set.add(new HostAndPort("139.196.205.76", 7003));
+        set.add(new HostAndPort("139.196.205.76", 7004));
+        set.add(new HostAndPort("139.196.205.76", 7005));
+        set.add(new HostAndPort("139.196.205.76", 7006));
+        JedisCluster jedisCluster = new JedisCluster(set);
+        jedisCluster.set("wigen", "wigen123");
+        System.out.println(jedisCluster.get("wigen"));
     }
 
 }
