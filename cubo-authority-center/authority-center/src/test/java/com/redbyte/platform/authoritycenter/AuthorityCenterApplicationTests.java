4bf959a5b3e26926c472b4d6922a8176fed3c825
18070900 <zett96@163.com>
2021-03-06 20:53:15 +0800

    test

--- cubo-authority-center/authority-center/src/test/java/com/redbyte/platform/authoritycenter/AuthorityCenterApplicationTests.java
+++ cubo-authority-center/authority-center/src/test/java/com/redbyte/platform/authoritycenter/AuthorityCenterApplicationTests.java
@@ -1,16 +1,25 @@
 package com.redbyte.platform.authoritycenter;
 
+import com.redbyte.platform.authoritycenter.config.RedisClusterConfigProperties;
 import com.redbyte.platform.authoritycenter.core.service.RegisterService;
 import com.redbyte.platform.authoritycenter.domain.RegisterUserDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootTest
 class AuthorityCenterApplicationTests {
 
     @Autowired
     private RegisterService registerService;
+    @Autowired
+    private RedisClusterConfigProperties redisClusterConfigProperties;
 
     @Test
     void contextLoads() throws Exception {
@@ -35,4 +44,22 @@ class AuthorityCenterApplicationTests {
         }
     }
 
+
+    @Test
+    void testRedisCluster() {
+        Set<HostAndPort> nodes = new HashSet<>();
+        for (String item : redisClusterConfigProperties.getNodes()) {
+            String[] items = item.split(":");
+            nodes.add(new HostAndPort(items[0], Integer.parseInt(items[1])));
+        }
+
+        JedisCluster jedisCluster = new JedisCluster(nodes);
+        jedisCluster.get("wigen");
+    }
+
+    @Test
+    void testRedis() {
+        Jedis jedis = new Jedis("139.196.205.76",7001);
+        jedis.get("wigen");
+    }
 }
