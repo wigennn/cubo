d02c225a429017c9c8e9fbfe5a5e0bd8c32c4333
wangwq <wangweiqing@yunzhangfang.com>
2021-03-10 18:01:17 +0800

    gateway

---
+++ cubo-gateway-center/src/main/java/com/redbyte/platform/cubogatewaycenter/CuboGatewayCenterApplication.java
@@ -0,0 +1,13 @@
+package com.redbyte.platform.cubogatewaycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuboGatewayCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuboGatewayCenterApplication.class, args);
+    }
+
+}
---
+++ cubo-gateway-center/src/test/java/com/redbyte/platform/cubogatewaycenter/CuboGatewayCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redbyte.platform.cubogatewaycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuboGatewayCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
